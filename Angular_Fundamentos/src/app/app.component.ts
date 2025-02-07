684c85e672db6c7c8fac3f1385312560f437d412
Marcio Luiz dos Santos Pereira <133532252+MarcioLuizBR@users.noreply.github.com>
2025-02-06 23:56:56 -0300

    componentes01

--- Angular_Fundamentos/src/app/app.component.ts
+++ Angular_Fundamentos/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { ComponenteFilhoComponent } from './components/componente-filho/componente-filho.component';
 import { ComponentePaiComponent } from './components/componente-pai/componente-pai.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, ComponenteFilhoComponent, ComponentePaiComponent],
+  imports: [RouterOutlet, HomeComponent, ComponentePaiComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
